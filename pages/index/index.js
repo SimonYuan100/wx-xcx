f1becb4e67655d01675e713ed630d67a4c3cfcab
SimonYuan <simonyuan100@163.com>
2017-09-08 21:04:50 +0800

    v0.2

--- pages/index/index.js
+++ pages/index/index.js
@@ -22,7 +22,8 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg'
-    ]
+    ],
+    textArr: ['竹炭', '孜然', '竹签', '米粉', '米皮']
   },
   //事件处理函数
   bindViewTap: function() {

7c88010b0bd5d59c3e5541cff75fea568a644b51
simonyuan100 <simonyuan100@163.com>
2017-09-11 01:07:22 +0800

    add 图片

--- pages/about/about.js
+++ pages/about/about.js
@@ -5,6 +5,37 @@ Page({
     comName: '强中鑫贸易',
     brief: '机制竹炭 | 新疆孜然',
     address: '深圳市深南大道55号',
-    map: '地图'
+    map: '地图',
+    markers: [{
+      iconPath: "../../images/marker.png",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 50,
+      height: 50
+    }],
+    polyline: [{
+      points: [{
+        longitude: 113.3245211,
+        latitude: 23.10229
+      }, {
+        longitude: 113.324520,
+        latitude: 23.21229
+      }],
+      color: "#FF0000DD",
+      width: 2,
+      dottedLine: true
+    }],
+    controls: [{
+      id: 1,
+      iconPath: '/images/marker.png',
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
   }
 })
